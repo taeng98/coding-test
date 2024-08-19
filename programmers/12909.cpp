373668664954cbf02bfe9e837a7bd6227b302d12
taeng98 <pth9080@gmail.com>
2024-08-19 22:20:39 +0900

    Add 12909.cpp

---
+++ programmers/12909.cpp
@@ -0,0 +1,33 @@
+// 12909번, 올바른 괄호
+// 자료구조
+
+/*
+스택의 개념을 익히는 데 좋은 문제였다.
+*/
+
+#include <string>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    stack<char> my_stack;
+
+    for (auto& ch : s) {
+
+        if (ch == '(') {
+            my_stack.push(ch);
+        }
+        else {
+            if (my_stack.empty() || my_stack.top() == ')') {
+                return false;
+            }
+            else {
+                my_stack.pop();
+            }
+        }
+    }
+
+    return my_stack.empty();
+}
